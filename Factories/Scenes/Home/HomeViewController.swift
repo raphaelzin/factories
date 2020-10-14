9e51382ba321b98e044424c6bea4008f27c0a008
Raphael Souza <raphael.pinheiro.souza@gmail.com>
2020-10-13 21:47:54 -0400

    Reorganize Scene group and remove unnecessary code

---
+++ Factories/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  Factories
+//
+//  Created by Raphael Souza on 2020-10-10.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    // MARK: Attibutes
+    
+    // MARK: Subviews
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
---
+++ Factories/Scenes/Home/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewModel.swift
+//  Factories
+//
+//  Created by Raphael Souza on 2020-10-13.
+//
+
+import Foundation
+
+protocol HomeViewModelType: class {
+    
+}
+
+class HomeViewModel: HomeViewModelType {
+    
+    // MARK: Attributes
+    
+    var 
+    
+}

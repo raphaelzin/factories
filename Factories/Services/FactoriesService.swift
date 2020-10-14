a29b0128ed1034f8eb7c05c223055093fc4141ef
Raphael Souza <raphael.pinheiro.souza@gmail.com>
2020-10-13 21:48:12 -0400

    Implements Factory Service

---
+++ Factories/Services/FactoriesService.swift
@@ -0,0 +1,39 @@
+//
+//  FactoriesService.swift
+//  Factories
+//
+//  Created by Raphael Souza on 2020-10-13.
+//
+
+import Foundation
+
+protocol FactoryServiceProvider: class {
+    func fetchFactories(next: String?, completion: @escaping FactoriesResponse)
+}
+
+typealias FactoriesResponse = (Result<PageResource<Factory>, Error>) -> Void
+
+class FactoryService: FactoryServiceProvider {
+    
+    // MARK: Attributes
+    
+    private let networkManager: NetworkClient
+    
+    // MARK: Lifecycle
+    
+    // Ensures dependency injection can be made
+    init(networkManager: NetworkClient = NetworService()) {
+        self.networkManager = networkManager
+    }
+    
+    // MARK: Service methods
+    
+    func fetchFactories(next: String?, completion: @escaping FactoriesResponse) {
+        
+        // Kinda weird not to have a path for the resources, but it works jsut fine.
+        networkManager.request(PageResource<Factory>.self,
+                               endpoint: .init(method: .get, path: ""),
+                               completion: completion)
+    }
+    
+}
